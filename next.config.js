031034821b2e0eb705efe7e5d5382ba323f3b2e0
BarbaraRM <bromeromachare@gmail.com>
2025-05-04 23:56:27 -0700

    eliminaciom aaa

--- next.config.js
+++ next.config.js
@@ -2,6 +2,9 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: false,
+  eslint: {
+    ignoreDuringBuilds: true, // 👈 esto evita que ESLint detenga el build en producción
+  },
 };
 
 module.exports = nextConfig;
