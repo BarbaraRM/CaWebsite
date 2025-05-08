1f8971e3dda1ca85a10cfa7701509944a5a12b2a
BarbaraRM <bromeromachare@gmail.com>
2025-05-08 07:00:15 -0700

    fallback data change

--- app/quienes-somos/page.tsx
+++ app/quienes-somos/page.tsx
@@ -9,7 +9,8 @@ async function getData(): Promise<any> {
     const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/admin-panel/about`, {
       ...(isDev
         ? { cache: "no-store" }
-        : { next: { revalidate: 60 } }),//60
+        : { cache: "no-store" }
+      )//{ next: { revalidate: 60 } }),//60
     });
     if (!res.ok) throw new Error("Respuesta no OK");
     const data = await res.json();
